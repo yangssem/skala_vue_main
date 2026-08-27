273256591932080726730a19af6c56dad336a5cf
yangssem88 <yangssem88@gmail.com>
2026-08-27 13:31:13 +0900

    axios2 third App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,30 @@
 <script setup>
-import WeatherMockup from './components/exercise/WeatherMockup.vue'
-import WeatherComposition from './components/exercise/WeatherComposition.vue'
-import WeatherParent from './components/exercise/WeatherParent.vue'
-import UnitToggler from './components/exercise/UnitToggler.vue'
+import StoreCounter from './components/practices/library/StoreCounter.vue'
+import AxiosWeather from './components/practices/library/AxiosWeather.vue'
+import AxiosJson from './components/practices/library/AxiosJson.vue'
+import ElementPlus from './components/practices/library/ElementPlus.vue'
+import EcmaScript from './components/practices/library/EcmaScript.vue'
 </script>
 
 <template>
-  <div class="app-container">
-    <h1>⛅ 과제 1: 날씨 (Mockup)</h1>
+  <div class="practice-container">
+    <h1>1. Store</h1>
     <hr />
-    <WeatherMockup />
-  </div>
-  <div class="app-container">
-    <h1>⛅ 과제 2: 날씨 (컴포지션)</h1>
+    <StoreCounter />
+    <h1>2. Axios</h1>
     <hr />
-    <WeatherComposition />
-  </div>
-  <div class="app-container">
-    <h1>⛅ 과제 3: 날씨 (컴포넌트)</h1>
+    <AxiosWeather />
+    <AxiosJson />
+    <h1>3. Element Plus</h1>
     <hr />
-    <WeatherParent />
-  </div>
-  <div class="app-container">
-    <h1>⛅ 과제 4: 라우터적용</h1>
+    <ElementPlus />
+    <h1>4. Modern JavaScript</h1>
     <hr />
-    <div class="dashboard-wrapper">
-      <nav class="navigation-bar">
-        <RouterLink to="/" class="nav-item">🌦️ 날씨 대시보드</RouterLink>
-        <span class="divider">|</span>
-        <RouterLink to="/about" class="nav-item">ℹ️ 서비스 소개</RouterLink>
-      </nav>
-      <main>
-        <RouterView />
-        <!-- RouterView v-slot="{ Component }">
-          <KeepAlive>
-            <component :is="Component" />
-          </KeepAlive>
-        </RouterView-->
-      </main>
-    </div>
-  </div>
-  <div class="app-container">
-    <h1>⛅ 과제 5: 스토어적용</h1>
-    <hr />
-    <div class="dashboard-wrapper">
-      <nav class="navigation-bar">
-        <RouterLink to="/" class="nav-item">🌦️ 날씨 대시보드</RouterLink>
-        <span class="divider">|</span>
-        <RouterLink to="/about" class="nav-item">ℹ️ 서비스 소개</RouterLink>
-        <UnitToggler />
-      </nav>
-      <main>
-        <RouterView />
-      </main>
-    </div>
+    <EcmaScript />
   </div>
 </template>
 
 <style>
 /* ⚠️ 외부 스타일 파일(예: 버튼 디자인 뭉치)을 이 방 안으로 쏙 가리켜 가져옵니다 */
-@import '@/assets/exercise.css';
+@import '@/assets/practice.css';
 </style>
